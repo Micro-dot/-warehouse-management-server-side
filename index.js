4884efd759d513db6d1dd248f17635d16bca796a
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-04-30 23:28:20 +0600

    Put Method added

--- index.js
+++ index.js
@@ -40,6 +40,20 @@ async function run() {
             res.send(result);
         })
 
+        app.put('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateQuantity = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updateQuantity.quantity
+                }
+             }
+            const result = await bookItemCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
     }
     finally {
 
