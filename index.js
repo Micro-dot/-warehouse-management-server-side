e20e4911b8b0f6321cb33858ba1e5a8fe69791c3
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-04-30 16:46:09 +0600

    Add Get and Post code

--- index.js
+++ index.js
@@ -5,11 +5,55 @@ const cors = require('cors');
 app.use(cors());
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.get ('/', (req,res) =>{
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qp1lk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log("Connected")
+async function run() {
+    try {
+        await client.connect();
+        const bookItemCollection = client.db('books').collection('items');
+        // Data load in mongodb Server
+        app.get('/items', async (req, res) => {
+            const query = {}
+            const cursor = bookItemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items)
+        });
+
+        // Get post by individual ID
+        app.get('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const items = await bookItemCollection.findOne(query);
+            res.send(items);
+        })
+
+        // Data Post in My UI
+        app.post('/items', async (req, res) => {
+            const allItems = req.body;
+            const result = await bookItemCollection.insertOne(allItems);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
     res.send("server is running")
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("Server Running", port);
 })
